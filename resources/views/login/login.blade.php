db46116a2402843bd2432a10296159fddc5b51f2
Imam Shofiuddin <imamshofiuddin00@gmail.com>
2021-12-29 17:55:38 +0700

    menambahkan tampilan login

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Page</title>
+</head>
+<body>
+    <h1>Login E-Voting</h1>
+    <form>
+        <label for="user">Username</label> :
+        <input type="text" name="user" id="user"><br>
+        <label for="pass">Password</label> :
+        <input type="password" name="" id="">
+    </form>
+</body>
+</html>
